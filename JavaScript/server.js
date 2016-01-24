634648c1c7e7409a8c40967ee3dacccfb5415c61
barakyaari <barakyaari@gmail.com>
2016-01-24 23:51:17 +0200

    deleted dbconnector

--- JavaScript/server.js
+++ JavaScript/server.js
@@ -1,7 +1,12 @@
 var mysql = require('mysql');
 var express = require('express');
 var app = express();
+
+var controllers = require('./controllers');
+controllers.set(app);
 var SERVER_PORT = 8080;
+process.env['accountname'] = 'chooserstorage';
+process.env['accountkey'] = 'rFb94KokcSFjPQJflCWmy9t8AqAM7rWdeUNYzGfiaEPKsY8kO2Lm2tF8fgEHsLIVCjGMYlYVP++vQ78+tYpV5A==';
 
 var connection = mysql.createConnection({
     host     : 'localhost',
@@ -11,57 +16,9 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-
-app.get('/', function (req, res) {
-	res.send('Hello World!');
-});
-
-app.get('/getPosts', function (req, res) {
-    var sql = 'SELECT id, title, image1, description1, image2, description2 from posts';
-
-    connection.query(sql, function(err, rows, fields) {
-        if (!err) {
-            console.log('Request Received');
-
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.send(JSON.stringify(rows));
-        }
-        else {
-            console.log('Error while performing Query: %s', sql);
-            console.log(err);
-            res.send(err);
-        }
-    });
-});
-
-app.get('/addPost', function (req, res) {
-    console.log("Request received: " + req);
-    var sql = "INSERT INTO posts (title, image1, description1, image2, description2, upload_time)" +
-        " VALUES ('"
-            + req.query.title + "', '"
-            + req.query.image1 + "', '"
-            + req.query.description1 + "', '"
-            + req.query.image2 + "', '"
-            + req.query.description2 + "', "
-            + "NOW())";
-    console.log("Got SQL Query: \n" + sql);
-    connection.query(sql, function(err, rows, fields) {
-        if (!err) {
-            console.log('Request Received');
-
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.send(JSON.stringify(rows));
-        }
-        else {
-            console.log('Error while performing Query: %s', sql);
-            console.log(err);
-            res.send(err);
-        }
-    });
-});
-
 var server = app.listen(SERVER_PORT, function () {
     var port = server.address().port;
 
     console.log('Listening on port %s', port);
 });
+
